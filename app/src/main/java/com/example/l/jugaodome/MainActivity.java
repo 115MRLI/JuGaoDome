3ff1ee4184067316ac60ea59f1660e8c18c134b8
115MRLI <1159642490@qq.com>
2018-11-27 19:19:56 +0800

    网络请求类

--- app/src/main/java/com/example/l/jugaodome/MainActivity.java
+++ app/src/main/java/com/example/l/jugaodome/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.l.jugaodome;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.l.jugaodome.base.BaseActivity;
@@ -24,8 +25,27 @@ public class MainActivity extends BaseActivity implements MainView {
     //开始请求
     @BindView(R.id.start)
     Button start;
+    //次数
+    @BindView(R.id.number)
+    EditText number;
+    //总次数
+    @BindView(R.id.all)
+    TextView all;
+    //成功次数
+    @BindView(R.id.success_number)
+    TextView successNumber;
+    //一次运行多少ci
+    @BindView(R.id.once_number)
+    EditText onceNumber;
+    //间隔时间
+    @BindView(R.id.once_time)
+    EditText onceTime;
+    //一个ip运行几次
+    @BindView(R.id.ip_number)
+    EditText ipNumber;
     //获取到的ipd地址
     private String ipadressstr = "";
+
     private MainPresenter presenter;
 
     @Override
@@ -45,7 +65,11 @@ public class MainActivity extends BaseActivity implements MainView {
         super.initEvent();
         ipadressstr = Utils.getIP(this);
         ipAddress.setText(ipadressstr);
-        Log.e("getOperatorName", Utils.getOperatorName(this));
+        number.setText("40");
+        onceNumber.setText("20");
+        onceTime.setText("30");
+        ipNumber.setText("10");
+        Log.e("getOperatorName", Utils.getOperatorName(this)+"            "+android.os.Build.MODEL+"          "+ android.os.Build.MANUFACTURER);
         jumpMain2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,7 +80,7 @@ public class MainActivity extends BaseActivity implements MainView {
             @Override
             public void onClick(View v) {
                 try {
-                    presenter.requestAdvertisement("com.jieanda.quanguossgjcx", Utils.changeURLEncoding("全国公交查询"), Utils.changeURLEncoding(Utils.getUserAgent(MainActivity.this)), "1.1.0", ipadressstr, "nokia", "n70", Utils.getIMEI(MainActivity.this), 720, 1280);
+                    presenter.requestAdvertisement("com.jieanda.quanguossgjcx", Utils.changeURLEncoding("全国公交查询"), Utils.changeURLEncoding(Utils.getUserAgent(MainActivity.this)), "1.1.0", ipadressstr, " MI 8 SE", "Xiaomi                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ", Utils.getIMEI(MainActivity.this), 720, 1280);
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
--- app/src/main/java/com/example/l/jugaodome/presenter/MainPresenter.java
+++ app/src/main/java/com/example/l/jugaodome/presenter/MainPresenter.java
@@ -12,7 +12,7 @@ public interface MainPresenter<T extends BaseView> extends BasePresenter<T> {
      * @param ua      系统webview的user-agent  需要 UrlEncode 处理
      * @param appv    应用版本号  例如：1.1.1
      * @param ip      请求设备的公网出口IPv4 地址  例如：211.136.28.18
-     * @param brand   手机设备厂商， 例如：nokia，samsung
+     * @param brand                                                                       ， 例如：nokia，samsung
      * @param model   手机设备型号  例如：n70，galaxy
      * @param uuid    设备唯一标识  对于 IOS 设备，该值为 idfa, 对于 Android 设备，该值为 imei
      * @param pw      备物理屏幕宽度  例如：720
